6958df0c207ac2520fdb47f4d858583a2694dcae
Deniskada <d.novikov@list.ru>
2025-10-22 01:08:32 +0300

    Исправление: импорт JSONResponse и Form в manager_payroll

--- apps/web/routes/manager_payroll.py
+++ apps/web/routes/manager_payroll.py
@@ -1,7 +1,7 @@
 """Роуты для работы управляющих с начислениями и выплатами."""
 
-from fastapi import APIRouter, Request, Depends, HTTPException, status, Query
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter, Request, Depends, HTTPException, status, Query, Form
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_
 from sqlalchemy.orm import selectinload
