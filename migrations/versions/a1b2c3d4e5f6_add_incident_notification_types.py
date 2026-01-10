391b02bfbc639acd11715ed4d417c0e57f4f6d9a
Deniskada <denis.novikov@list.ru>
2026-01-10 19:57:11 +0300

    Исправление порядка миграций: инциденты после унификации enum

--- migrations/versions/a1b2c3d4e5f6_add_incident_notification_types.py
+++ migrations/versions/a1b2c3d4e5f6_add_incident_notification_types.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = 'ce60fdd16bba'
+down_revision: Union[str, None] = 'ffaa1ceacd63'  # Исправлено: после унификации enum значений
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
